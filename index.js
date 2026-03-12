a7e49ad9a3e3670f49f606a09b987a0e5d46a1e9
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-03-12 15:06:37 +0500

    chaning origin of cookie

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ import cookieParser from "cookie-parser";
 const app = express();
 mongoconnect();
 
-
-app.set("trust proxy", 1);
 // midleware 
-app.use( cors({
-  origin: "https://ecomerence-website-six.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
-}
-))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  }),
+);
 
 // app.set("view engine","ejs")
 app.use(express.urlencoded({ extended: false }));
