7c49a4ab039058fc5b26f6a24764b03ff80664d8
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-03-25 11:49:16 +0500

    adding cart feature and a little work has done on order feature

--- Modals/cartschema.js
+++ Modals/cartschema.js
@@ -1,13 +1,2 @@
 import mongoose from "mongoose";
 
-const cartschema = mongoose.Schema({
-  cart: [
-    {
-          product: { type: mongoose.Schema.Types.ObjectId, ref: "Products" },
-          quantity: Number
-    }
-  ]
-});
-
-const Cart = mongoose.model("Cart", cartschema);
-export default Cart;
