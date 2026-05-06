3448b16433dcbd8b73c57ecb7b7ab045d8ced8f5
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-05-06 14:23:19 +0500

    adding one more origin in cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoconnect();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin:"https://ecomerence-website-six.vercel.app",
+    origin:["https://ecomerence-website-six.vercel.app", "http://localhost:5173/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   }),
