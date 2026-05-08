658e71b2e0902e4d0baa835100d4adb2ba931e3d
khadijaZafar2511 <khadijazafar2511@gmail.com>
2026-05-08 09:06:34 +0500

    temporary setting for local host

--- Controller/auth.js
+++ Controller/auth.js
@@ -57,13 +57,13 @@ const login = async (req, res) => {
       httpOnly: true,
       secure: true, // Must be true for HTTPS/deployed
       sameSite: "none", // Must be "none" for cross-site cookies
-      maxAge: 86400,
+      maxAge: 86400000,
     });
        res.cookie("isLoggedIn", "true", {
       httpOnly: false,
-      secure: true, // Must be true for HTTPS/deployed
-      sameSite: "none", // Must be "none" for cross-site cookies
-      maxAge: 86400,
+      secure: false, // Must be true for HTTPS/deployed
+      sameSite: "lax", // Must be "none" for cross-site cookies
+      maxAge: 86400000,
     });
     res.status(200).json({ message: "Login successful!" });
   } catch (err) {
